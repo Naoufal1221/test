271f52d9a0ed48a44a1d82b039311d18ca8bec75
Naoufal Sarf <naoufal.stage>
2020-08-03 01:22:20 +0100

    updatee

--- ATourelecPages/script.js
+++ ATourelecPages/script.js
@@ -172,10 +172,28 @@ lastSousList.addEventListener('mouseleave',function(){
   setTimeout(function(){lastInner.style.opacity=0;},200);
 });
 
-const vidAcceuil = document.getElementById('vid');
 
-vidAcceuil.addEventListener('click',function(){
-  vidAcceuil.pause();
+
+
+
+const filt = document.getElementById('filtreByYear');
+const filtByMarket = document.getElementById('filtreByMarket');
+const allProjects = document.querySelectorAll('.project');
+
+filt.addEventListener('click',function(){
+   const result=document.getElementById('mySelectYear').value;
+   console.log('value is '+ result);
+   allProjects.forEach(proj => proj.style.display='none');
+  document.querySelectorAll('.p-'+ result).forEach(p => p.style.display='block');
+
+})
+
+filtByMarket.addEventListener('click',function(){
+  const result = document.getElementById('mySelectMarket').value;
+  console.log('Value is '+result);
+  allProjects.forEach(proj => proj.style.display='none');
+  document.querySelectorAll('.m-'+ result).forEach(p => p.style.display='block');
+
 })
 
 
---
+++ ATourelecPages/treatement.js
@@ -0,0 +1,5 @@
+const vidAcceuil = document.getElementById('vid');
+
+vidAcceuil.addEventListener('click',function(){
+  vidAcceuil.pause();
+})
